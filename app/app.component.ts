26bcf674f0900497cafa1cd81cc4cc5addb580ec
Lise Karoliussen Haugen <lhaugen@capgemini.com>
2017-01-10 15:31:52 +0100

    removed test components

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,15 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <div class="page-container">
-    <div class="row">
-
-        
+  <div class="container">
+    <div class="row"> 
       <h1>Hello {{name}}</h1>
-      
-      <sidebar></sidebar>
-      
-      <hello></hello>
+
     </div>
   </div>
   `,
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
-import { HelloComponent } from './components/hello/hello.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, HelloComponent, SidebarComponent ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
